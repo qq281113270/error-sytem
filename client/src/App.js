e10cee9f1b4749fb6192d19a5e9ba8a22ef4d433
qq281113270 <yao guan shou>
2020-08-30 19:26:47 +0800

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Router from './router/index';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
- 
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+import './index.css';
+
+moment.locale('zh-cn');
 function App() {
   return (
     <div  >
